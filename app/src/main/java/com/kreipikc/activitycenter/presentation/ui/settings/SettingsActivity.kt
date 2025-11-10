a0510149a8d68e7817cdbb4977cae396f2d0719e
kreipikc <basmenov5kir@gmail.com>
2025-11-10 22:31:51 +0300

    small fix

--- app/src/main/java/com/kreipikc/activitycenter/presentation/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/kreipikc/activitycenter/presentation/ui/settings/SettingsActivity.kt
@@ -5,11 +5,11 @@ import android.widget.ImageButton
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.kreipikc.activitycenter.R
-import com.kreipikc.activitycenter.databinding.ActivityMainBinding
+import com.kreipikc.activitycenter.databinding.ActivitySettingsBinding
 import com.kreipikc.activitycenter.presentation.ui.base.BaseActivity
 import com.kreipikc.activitycenter.presentation.ui.main.MainActivity
 
-class SettingsActivity : BaseActivity<ActivityMainBinding>() {
+class SettingsActivity : BaseActivity<ActivitySettingsBinding>() {
     override fun getLayoutRes(): Int = R.layout.activity_settings
 
     override fun setupContent() {
